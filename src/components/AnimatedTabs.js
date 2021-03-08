97e31b0775b36bea53c7e5d14ee42c7ad6ed66f5
YasirSherzad <ys.sherzad@gmail.com>
2021-03-08 09:26:44 +0430

    add AnimatedTabs

---
+++ src/components/AnimatedTabs.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { View } from 'react-native';
+
+function Animatedtabs(props) {
+
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {}
+});
+
+export default Animatedtabs;
