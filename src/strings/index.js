90a1b500bfa920ea018c4bde4c9699b90b52653b
YasirSherzad <ys.sherzad@gmail.com>
2021-03-08 09:29:19 +0430

    change header title

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,5 +1,5 @@
 export default {
-    imageHeaderTitle: 'netlinks media',
+    imageHeaderTitle: 'travel',
     imageContentTitle: 'Get ready for your\nlifetime journey!',
     imageContentSubTitle: 'Collection of the most beautiful places, experiences,\nand unusual housings in Afghanistan',
     getStarted: 'Get Started',
