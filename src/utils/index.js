74eaf5fd35d35380bf04f893628eb0bd0d74d1e7
YasirSherzad <ys.sherzad@gmail.com>
2021-03-13 22:27:08 +0430

    add theme colors

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,9 @@ import { Dimensions } from 'react-native';
 export const WIDTH = Dimensions.get('window').width;
 
 export const HEIGHT = Dimensions.get('window').height;
+
+export const colors = {
+    primary: '#F75D36',
+    inactive: '#CBCBCB',
+    border: '#F8F6F7'
+};
