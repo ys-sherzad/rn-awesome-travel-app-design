80eb5ad473ad826adec274194b561e212c429fb5
YasirSherzad <ys.sherzad@gmail.com>
2021-03-13 22:25:30 +0430

    add animated tabs

--- src/components/home/AnimatedTabs.component.js
+++ src/components/home/AnimatedTabs.component.js
@@ -1,18 +1,116 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, TouchableWithoutFeedback } from 'react-native';
 import { View } from 'react-native';
+import Animated, { useSharedValue, useAnimatedStyle, withTiming, Easing } from 'react-native-reanimated';
+import { colors } from '../../utils';
 
-function Animatedtabs(props) {
+const tabs = [
+    { id: 0, name: 'Experiences' },
+    { id: 1, name: 'Places' },
+    { id: 2, name: 'Housings' },
+];
+
+const CONTAINER_WIDTH = 150 * 3;
+const TAB_WIDTH = 150;
+
+function AnimatedTabs(props) {
+    const offset = useSharedValue(0);
+
+    const [selectedTabId, setSelectedTabId] = useState(0);
+
+    const animatedX = useAnimatedStyle(() => {
+        return {
+            transform: [{
+                translateX: withTiming(offset.value, {
+                    duration: 250,
+                    easing: Easing.inOut(Easing.sin)
+                })
+            }]
+        };
+    });
+
+    useEffect(() => {
+        animate();
+    }, [selectedTabId]);
+
+    const animate = () => {
+        let animateToValue;
+
+        if (selectedTabId === 0) {
+            animateToValue = 0;
+        }
+        if (selectedTabId === 1) {
+            animateToValue = TAB_WIDTH;
+        }
+        if (selectedTabId === 2) {
+            animateToValue = TAB_WIDTH * 2;
+        }
+        offset.value = animateToValue;
+    };
+
+
+    const tabButton = (tab) => {
+        const textStyle = tab.id === selectedTabId ?
+            StyleSheet.flatten([styles.tabText, { color: colors.primary }])
+            : styles.tabText;
+        return (
+            <TouchableWithoutFeedback onPress={() => setSelectedTabId(tab.id)}>
+                <View style={styles.tabBtn}>
+                    <Text style={textStyle}>{tab.name}</Text>
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    };
+
+
+    const dotIndicator = () =>
+        <Animated.View style={[styles.dotContainer, animatedX]}>
+            <View style={styles.dot} />
+        </Animated.View>;
 
     return (
-        <View>
+        <View style={styles.container}>
+            <View style={styles.tabsContainer}>
+                {tabs.map((tab) => tabButton(tab))}
+            </View>
 
+            {dotIndicator()}
         </View>
     );
 }
 
+
 const styles = StyleSheet.create({
-    container: {}
+    container: {
+        height: 50,
+        width: CONTAINER_WIDTH,
+        justifyContent: 'center',
+        marginHorizontal: 35
+    },
+    tabsContainer: {
+        flexDirection: 'row',
+    },
+    tabBtn: {
+        height: '100%',
+        width: TAB_WIDTH,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    tabText: {
+        fontFamily: 'Montserrat-SemiBold',
+        fontSize: 20,
+        color: colors.inactive
+    },
+    dotContainer: {
+        width: TAB_WIDTH,
+        alignItems: 'center',
+    },
+    dot: {
+        height: 6,
+        width: 6,
+        backgroundColor: colors.primary,
+        borderRadius: 10 / 2,
+    }
 });
 
-export default Animatedtabs;
+export default AnimatedTabs;
