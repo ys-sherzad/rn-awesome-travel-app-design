82181bcd2018ee538e094efa5badb2ebc6b15fd9
YasirSherzad <ys.sherzad@gmail.com>
2021-03-25 21:55:20 +0430

    add left and right arrow functions

--- src/components/home/ScrollableCards.component.js
+++ src/components/home/ScrollableCards.component.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { StyleSheet, FlatList, View } from 'react-native';
 import Card from './Card.component';
 import * as FakeData from '../../fake_data';
@@ -6,6 +6,8 @@ import { colors, PIXEL_RATIO } from '../../utils';
 import Arrows from '../shared/Arrows.component';
 
 function Scrollablecards(props) {
+    const flatListRef = useRef();
+    const [animatedCurrentIndex, setAnimatedCurrentIndex] = useState(0);
 
     const renderItem = ({ item, index }) =>
         <Card {...{ item }} {...{ index }} />;
@@ -14,11 +16,33 @@ function Scrollablecards(props) {
 
     const ItemSeparatorComponent = () => <View style={styles.cardSeparator} />;
 
-    console.log({ PIXEL_RATIO });
+    const onLeftArrowPress = useCallback(() => {
+        console.log('left pressed');
+    }, []);
+
+    const onRightArrowPress = () => {
+
+        if (flatListRef.current) {
+            flatListRef.current.scrollToIndex({
+                animated: true,
+                index: 4
+            });
+        }
+    };
+
+    // const getIndexToAnimate = () => {
+    //     const dataLength = FakeData.data.length;
+
+    // }
+
+    getItemLayout = (data, index) => (
+        { length: 260, offset: 260 * index, index }
+    );
 
     return (
         <View>
             <FlatList
+                ref={flatListRef}
                 horizontal
                 data={FakeData.data}
                 {...{ renderItem }}
@@ -26,10 +50,14 @@ function Scrollablecards(props) {
                 {...{ ItemSeparatorComponent }}
                 contentContainerStyle={styles.flatlistContentContainer}
                 showsHorizontalScrollIndicator={false}
+                initialNumToRender={2}
+                {...{ getItemLayout }}
             />
 
             <View style={styles.arrowsContainer}>
                 <Arrows
+                    {...{ onLeftArrowPress }}
+                    {...{ onRightArrowPress }}
                     leftArrowColor={colors.iconInactive}
                     rightArrowColor={colors.iconActive}
                     size={26}
