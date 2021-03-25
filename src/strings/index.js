05df50a9a118de64b530ade22adba3b1af467cfe
YasirSherzad <ys.sherzad@gmail.com>
2021-03-25 21:53:38 +0430

    update constants

--- src/strings/index.js
+++ src/strings/index.js
@@ -3,4 +3,9 @@ export default {
     imageContentTitle: 'Get ready for your\nlifetime journey!',
     imageContentSubTitle: 'Collection of the most beautiful places, experiences,\nand unusual housings in Afghanistan',
     getStarted: 'Get Started',
+    search: 'Search',
+    discover: 'Discover',
+    blog: 'Blog',
+    adventurous_mood: 'Adventurous Mood?',
+    show_all: 'Show all'
 }
