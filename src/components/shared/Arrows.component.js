b27376a40f3bd138cd1a9a6ec7ba20820a61eb0e
YasirSherzad <ys.sherzad@gmail.com>
2021-03-25 21:54:42 +0430

    make arrows as buttons

--- src/components/shared/Arrows.component.js
+++ src/components/shared/Arrows.component.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
 
 function Arrows({
     leftArrowColor,
     rightArrowColor,
-    size
+    size,
+    onLeftArrowPress,
+    onRightArrowPress
 }) {
     return (
         <View style={styles.container}>
-            <Feather
-                name='chevron-left'
-                size={size ?? 22}
-                color={leftArrowColor}
-            />
+            <TouchableOpacity onPress={onLeftArrowPress}>
+                <Feather
+                    name='chevron-left'
+                    size={size ?? 22}
+                    color={leftArrowColor}
+                />
+            </TouchableOpacity>
             <View style={styles.separator} />
-            <Feather
-                name='chevron-right'
-                size={size ?? 22}
-                color={rightArrowColor}
-            />
+            <TouchableOpacity onPress={onRightArrowPress}>
+                <Feather
+                    name='chevron-right'
+                    size={size ?? 22}
+                    color={rightArrowColor}
+                />
+            </TouchableOpacity>
         </View>
     );
 }
