6a56f02031ef6f1463a4004a30221620f9d9418e
YasirSherzad <ys.sherzad@gmail.com>
2021-03-29 09:37:23 +0430

    use Animated wrapper

--- src/components/home/Card.component.js
+++ src/components/home/Card.component.js
@@ -1,55 +1,37 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { StyleSheet, View, Image, Text } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import Feather from 'react-native-vector-icons/Feather';
-import Animated, { useSharedValue, useAnimatedStyle, withTiming, Easing, withDelay } from 'react-native-reanimated';
-import { WIDTH } from '../../utils';
+import AnimatedHorizontal from '../shared/AnimatedHorizontal.component';
 
-const gradientColors = ['rgba(0,0,0,0.1)', 'rgba(0,0,0,0.15)', 'rgba(0,0,0,0.6)'];
-
+const gradientColors = [
+    'rgba(0,0,0,0.1)',
+    'rgba(0,0,0,0.15)',
+    'rgba(0,0,0,0.6)'
+];
 
 function Card({ item, index }) {
     const { title, location, image } = item;
 
-    const offset = useSharedValue(WIDTH * .4);
-
-    const timingConfig = {
-        duration: 1000,
-        easing: Easing.inOut(Easing.cubic)
-    };
-
-    const animatedX = useAnimatedStyle(() => {
-        const dur = (index * 80) + 2300;
-        return {
-            transform: [{
-                translateX: withDelay(dur,
-                    withTiming(offset.value, timingConfig))
-            }]
-        };
-    });
-
-    useEffect(() => {
-        // animate();
-        offset.value = 0;
-    }, []);
-
     return (
-        <Animated.View style={[styles.container, animatedX]}>
+        <AnimatedHorizontal {...{ index }}>
+            <View style={styles.container}>
 
-            <Image source={require('../../../assets/image2.jpg')} style={styles.bgImage} />
+                <Image source={require('../../../assets/image2.jpg')} style={styles.bgImage} />
 
-            <LinearGradient colors={gradientColors} style={styles.gradientLayer} />
+                <LinearGradient colors={gradientColors} style={styles.gradientLayer} />
 
-            <View style={styles.textContent}>
-                <Text style={styles.titleText}>{title}</Text>
+                <View style={styles.textContent}>
+                    <Text style={styles.titleText}>{title}</Text>
 
-
-                <View style={styles.locationContainer}>
-                    <Feather name='map-pin' size={18} color='#fff' />
-                    <Text style={styles.locationText}>{location}</Text>
+                    <View style={styles.locationContainer}>
+                        <Feather name='map-pin' size={18} color='#fff' />
+                        <Text style={styles.locationText}>{location}</Text>
+                    </View>
                 </View>
+
             </View>
-        </Animated.View>
+        </AnimatedHorizontal>
     );
 }
 
